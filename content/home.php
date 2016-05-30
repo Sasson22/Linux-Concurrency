8c907d95a99894dbbb4b436a239cef12e770dd80
Valentin <valio953@gmail.com>
2016-05-30 19:25:07 +0200

    30.05.2016-Valio-home.php

--- content/home.php
+++ content/home.php
@@ -1,31 +1,13 @@
 ﻿<div class="row">
     <div id="div_interiorimg" class="col-md-12">
         <img id="img_interior" src="<?php echo MAIN_URL; ?>images/restourant_interior.jpg" />
+            <p id="p_intro_text">
+                Flavors and tastes of Melbourne in De Pijp
+            </p>
     </div>
 </div>
 
 <div class="container_content">
-    <div class="row">
-        <div class="col-md-12">
-            <p id="p_intro_text">
-                Little Collins, bar and restourant Amsterdam is an atribute to our home
-                city, Melbourne, and the many things we love to eat and drink.
-            </p>
-        </div>
-    </div>
-    
-   <!-- <div class="row">
-        <div class="col-md-4 food-badge">
-            
-        </div>
-        <div class="col-md-4">
-            
-        </div>
-        <div class="col-md-4">
-            
-        </div>
-    </div>-->
-   
    <div class="row">
         <div class="col-md-12 col-xs-12">
             <center><button id="btn_booknow">Book now!</button></center>
@@ -40,22 +22,22 @@
    
    <!-- Menu boxes -->
     <div class="row">
-        <div class="col-md-1"></div>
-        <div class="col-md-5 padding-r-5">
+        <div class="col-md-1 col-xs-1"></div>
+        <div class="col-md-5 col-xs-5 padding-r-5">
             <div id="div_drink_spirit"  class="menu-details pull-right">
                 <a href="#"><div>Drink</div></a>
                 <hr class="hr-menuboxes" />
                 <a href="#"><div>Spirit</div></a>
             </div>
         </div>
-        <div class="col-md-5 padding-l-5">
+        <div class="col-md-5 col-xs-5 padding-l-5">
             <div id="div_brunch_dinner" class="menu-details">
                 <a href="#"><div>Brunch</div></a>
                 <hr class="hr-menuboxes" />
                 <a href="#"><div>Dinner</div></a>
             </div>
         </div>
-        <div class="col-md-1"></div>
+        <div class="col-md-1 col-xs-1"></div>
     </div>
     <!-- End Menu boxes -->
     
@@ -64,4 +46,42 @@
             <a href=""></a>
         </div>
     </div>
+    
+    <div id="div_contact">
+        <div>Contact</div>
+        <div id="map_container" class="row">
+            <div class="col-md-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9747.034421547645!2d4.8934112!3d52.3566633!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6098df5e51337%3A0x9ab7a48b907a7189!2sLittle+Collins!5e0!3m2!1sbg!2sbg!4v1464615192089" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+            <div class="col-md-6">
+                <a href="mailto:info@littlecollins.nl">info@littlecollins.nl</a>
+                <p>+31(0) 20 6732293</p>
+                <p>1e Sweelinckstraat 19-F 1073 CL, Amsterdam</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div id="div_seats" class="col-md-6 col-xs-12">
+            <iframe width=600px height=500px frameborder=0 src="https://www.seatme.nl/srm/availabletimes.htm?rid=1612&af=1147&lang=en"></iframe>
+        </div>
+        <div class="col-md-6 col-xs-12">
+            <div id="div_gallery_txt">Gallery</div>
+            <img id="img_interior2" src="<?php echo MAIN_URL; ?>images/restourant_interior.jpg" />
+            <div id="slider_container">
+                <div class="slider-circle"></div>
+                <div class="slider-circle"></div>
+                <div class="slider-circle"></div>
+                <div class="slider-circle"></div>
+                <div class="slider-circle"></div>
+                <div class="slider-circle"></div>
+                <div class="slider-circle"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-xs-12">
+            <center><button id="btn_booknow">Book now!</button></center>
+        </div>
+   </div>
+    
 </div>
