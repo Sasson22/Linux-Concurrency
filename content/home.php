e7598c1eb283fc36ce78928a12dbc58d05db0e1c
Valentin <valio953@gmail.com>
2016-05-31 12:57:24 +0200

    31.05-Valio

--- content/home.php
+++ content/home.php
@@ -7,7 +7,6 @@
     </div>
 </div>
 
-<div class="container_content">
    <div class="row">
         <div class="col-md-12 col-xs-12">
             <center><button id="btn_booknow">Book now!</button></center>
@@ -22,38 +21,121 @@
    
    <!-- Menu boxes -->
     <div class="row">
-        <div class="col-md-1 col-xs-1"></div>
-        <div class="col-md-5 col-xs-5 padding-r-5">
+        <div class="col-xs-1 col-sm-1 hidden-md hidden-lg"></div>
+        <div class="col-xs-5 col-sm-5 padding-r-5 hidden-md hidden-lg">
             <div id="div_drink_spirit"  class="menu-details pull-right">
                 <a href="#"><div>Drink</div></a>
                 <hr class="hr-menuboxes" />
                 <a href="#"><div>Spirit</div></a>
             </div>
         </div>
-        <div class="col-md-5 col-xs-5 padding-l-5">
+        <div class="col-xs-5 col-sm-5 padding-l-5 hidden-md hidden-lg">
             <div id="div_brunch_dinner" class="menu-details">
                 <a href="#"><div>Brunch</div></a>
                 <hr class="hr-menuboxes" />
                 <a href="#"><div>Dinner</div></a>
             </div>
         </div>
-        <div class="col-md-1 col-xs-1"></div>
+        <div class="col-xs-1 col-sm-1 hidden-md hidden-lg"></div>
     </div>
     <!-- End Menu boxes -->
     
-    <div class="row">
-        <div class="col-md-12">
-            <a href=""></a>
+    <div id="row_menu_badges" class="row">
+        <div class="col-md-4 hidden-xs hidden-sm">
+            <div class="rmenu-badge">
+                <center>
+                    <div style="width: 80%;">
+                        <span class="headingtxt-rmenu-badge">Brunch</span>
+                        <img src="<?php echo MAIN_URL;?>images/brunch.jpg" class="img-rmenu-badge" />
+                        <p>
+                            Our tasty brunch menu is available from Wednesday to
+                            Sunday and includes all those brunches you have been
+                            desiring for so long.
+                        </p>
+                    </div>
+                </center> 
+            </div>
+        </div>
+        <div class="col-md-4 hidden-xs hidden-sm">
+            <div class="rmenu-badge">
+                <span class="headingtxt-rmenu-badge">Dinner</span>
+                <center>
+                    <div style="width: 80%;">
+                        <img src="<?php echo MAIN_URL;?>images/brunch.jpg" class="img-rmenu-badge" />
+                        <p>
+                            Our dinner menu, available Thursdays and Fridays, is a
+                            mouth-watering selection of contemporary foods from
+                            around the globe - lone diners can sample several dishes
+                            and guests in groups are encouraged to share.
+                        </p>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="col-md-4 hidden-xs hidden-sm">
+            <div class="rmenu-badge">
+                <span class="headingtxt-rmenu-badge">Drinks</span>
+                <center>
+                    <div style="width: 80%;">
+                        <img src="<?php echo MAIN_URL;?>images/brunch.jpg" class="img-rmenu-badge" />
+                        <p>
+                            If you prefer to join us for only a drink, then come and
+                            check out our beatiful bar and our international drinks
+                            and wine list - you might find something new!
+                        </p>
+                    </div>
+                </center>
+            </div>
         </div>
     </div>
     
     <div id="div_contact">
-        <div>Contact</div>
-        <div id="map_container" class="row">
-            <div class="col-md-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9747.034421547645!2d4.8934112!3d52.3566633!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6098df5e51337%3A0x9ab7a48b907a7189!2sLittle+Collins!5e0!3m2!1sbg!2sbg!4v1464615192089" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <div id="div_contact_txt">Contact</div>
+        <div id="contact_container" class="row">
+            <div class="col-md-6 col-xs-12 col-sm-12">
+                <center><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9747.034421547645!2d4.8934112!3d52.3566633!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6098df5e51337%3A0x9ab7a48b907a7189!2sLittle+Collins!5e0!3m2!1sbg!2sbg!4v1464615192089" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></center>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 col-xs-12 col-sm-12">
+                <div class="row">
+                    <div class="col-md-12 hidden-xs hidden-sm">
+                        <center>
+                            <table id="tbl_oppening_hours">
+                                <thead>
+                                    <tr>
+                                        <th id="tbl_heading" colspan="2">Openning Hours</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="td-day">Mon:</td>
+                                        <td>10:30 - 16:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="td-day">Wed:</td>
+                                        <td>10:30 - 22:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="td-day">Thu:</td>
+                                        <td>10:30 - 22:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="td-day">Fri:</td>
+                                        <td>10:30 - 22:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="td-day">Sat:</td>
+                                        <td>9:00 - 22:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="td-day">Sun:</td>
+                                        <td>9:00 - 22:00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </center>
+                    </div>
+                </div>
+                
                 <a href="mailto:info@littlecollins.nl">info@littlecollins.nl</a>
                 <p>+31(0) 20 6732293</p>
                 <p>1e Sweelinckstraat 19-F 1073 CL, Amsterdam</p>
@@ -62,10 +144,10 @@
     </div>
     
     <div class="row">
-        <div id="div_seats" class="col-md-6 col-xs-12">
-            <iframe width=600px height=500px frameborder=0 src="https://www.seatme.nl/srm/availabletimes.htm?rid=1612&af=1147&lang=en"></iframe>
+        <div id="div_seats" class="col-md-6 col-xs-12 col-sm-12">
+            <center><iframe width=600px height=500px frameborder=0 src="https://www.seatme.nl/srm/availabletimes.htm?rid=1612&af=1147&lang=en"></iframe></center>
         </div>
-        <div class="col-md-6 col-xs-12">
+        <div class="col-md-6 col-xs-12 col-sm-12">
             <div id="div_gallery_txt">Gallery</div>
             <img id="img_interior2" src="<?php echo MAIN_URL; ?>images/restourant_interior.jpg" />
             <div id="slider_container">
